dbb215da960a45c56ab70714f143375f5b8fad5f
liyulong <764861214@qq.com>
2024-03-22 22:19:47 +0800

    setup概述

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,12 @@
 <template>
-    <div class="app">
-        <h1>你好啊！</h1>
-    </div>
+    <Person/>
 </template>
 
 <script lang="ts">
-
-</script>
+    import Person from './components/Person.vue'
     export default {
-        name:'App' //组件名
+        name:'App', //组件名
+        components:{Person}
     }
-<style>
-    .app {
-        background-color: #ddd;
-        box-shadow: 0 0 10px;
-        border-radius: 10px;
-        padding: 20px;
-    }
-</style>
+</script>
+    
