3148599af7e8971ec7bb60256c2d87fa441892b0
liyulong <764861214@qq.com>
2024-03-24 22:12:17 +0800

    reactive创建对象类型的响应数据

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -5,16 +5,32 @@
     <button @click="changeName">修改名字</button>
     <button @click="changeAge">修改年龄</button>
     <button @click="showTel">查看联系方式</button>
+    <br>
+    <h2>资产：一辆{{car.brand}},价值{{car.price}}万</h2>
+    <button @click="chagePrice">修改汽车的价格</button>
+    <br>
+    <h2>爱好：</h2>
+    <ul>
+      <li v-for="g in games" :key="g.id">{{g.name}}</li>
+    </ul>
+    <button @click="changeFirstGame">修改第一个游戏的名字</button>
   </div>
 </template>
 
 <script lang="ts" setup name="Person">
   import { ref } from 'vue';
+  import { reactive } from 'vue';
 
   let name = ref('张三')
   let age = ref(18)
   let tel = '13888888888'
-  
+  let car = reactive({brand:'宝马',price:100})
+  let games = reactive([
+    {id:'li01',name:'三国志14'},
+    {id:'li02',name:'金铲铲'},
+    {id:'li03',name:'英雄联盟'}
+  ])
+
   // 方法
   function changeName() {
     name.value = 'zhang-san' 
@@ -26,6 +42,12 @@
   }
   function showTel() {
     alert(tel)
+  }
+  function chagePrice(){
+    car.price += 10
+  }
+  function changeFirstGame(){
+    games[0].name = '御龙在天'
   }
 </script>
 
