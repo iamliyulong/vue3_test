35c8aa057d204b992ddd71d1700eadcaa6b3f948
liyulong <764861214@qq.com>
2024-03-26 22:14:01 +0800

    computed计算属性使用

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
-    <Person/>
+    <!-- <Person/> -->
+    <Person2/>
 </template>
 
 <script lang="ts">
-    import Person from './components/Person.vue'
+    // import Person from './components/Person.vue'
+    import Person2 from './components/Person2.vue'
     export default {
         name:'App', //组件名
-        components:{Person}
+        // components:{Person}
+        components:{Person2}
     }
 </script>
     
---
+++ src/components/Person2.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="person2">
+    姓：<input type="text" v-model="firstName"><br>
+    名：<input type="text" v-model="lastName"><br>
+    全名：<input type="text" v-model="fullName"><br>
+    <button @click="chageFullName">修改全名为Li-si</button>
+  </div>
+</template>
+
+<script lang="ts" setup name="Person2">
+import { ref, computed } from 'vue';
+
+let firstName = ref('zhang')
+let lastName = ref('san')
+
+// fullName是一个计算属性，且是只读的
+// let fullName = computed(() => {
+//   return firstName.value.slice(0, 1).toUpperCase() + firstName.value.slice(1) + '-' + lastName.value
+// })
+
+// fullName是一个计算属性，可读可写
+let fullName = computed({
+  get() {
+    return firstName.value.slice(0, 1).toUpperCase() + firstName.value.slice(1) + '-' + lastName.value
+  },
+  set(val) {
+    const [str1, str2] = val.split('-')
+    firstName.value = str1
+    lastName.value = str2
+  }
+})
+
+function chageFullName() {
+  fullName.value = 'li-si'
+}
+
+</script>
+
+<style scoped>
+.person2 {
+  background-color: skyblue;
+  box-shadow: 0 0 10px;
+  border-radius: 10px;
+  padding: 20px;
+}
+
+button {
+  margin: 0 5px;
+}
+</style>
