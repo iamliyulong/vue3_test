0ede3c09280524b1fadc8eca134ad9f32dd19f3f
liyl <liyl02@superred.com.cn>
2024-03-27 11:20:58 +0800

    修改button样式

--- src/components/Person2.vue
+++ src/components/Person2.vue
@@ -48,25 +48,34 @@ function chageFullName() {
 
 /* 修改按钮边框和颜色 */
 button {
-  border: 1px solid #cccccc; /* 添加边框 */
-  background-color: #f5f5f5; /* 修改背景色 */
-  color: #1208a1; /* 修改字体颜色 */
-  padding: 8px 16px; /* 增加内边距 */
+  border: 1px solid #de0fc3;
+  /* 添加边框 */
+  background-color: #1ec577;
+  /* 修改背景色 */
+  color: #1208a1;
+  /* 修改字体颜色 */
+  padding: 8px 16px;
+  /* 增加内边距 */
   text-align: center;
   text-decoration: none;
   display: inline-block;
   font-size: 14px;
   margin: 4px 2px;
-  transition: all 0.3s; /* 添加过渡效果 */
-  cursor: pointer; /* 修改光标样式 */
-  border-radius: 4px; /* 添加圆角 */
+  transition: all 0.3s;
+  /* 添加过渡效果 */
+  cursor: pointer;
+  /* 修改光标样式 */
+  border-radius: 4px;
+  /* 添加圆角 */
 }
 
 /* 鼠标悬停时改变按钮样式 */
 button:hover {
-  background-color: #dddddd; /* 改变背景色 */
-  color: #e35151; /* 改变字体颜色 */
-  border-color: #af1c1c; /* 改变边框颜色 */
+  background-color: #c7de16;
+  /* 改变背景色 */
+  color: #e35151;
+  /* 改变字体颜色 */
+  border-color: #af1c1c;
+  /* 改变边框颜色 */
 }
-
 </style>
