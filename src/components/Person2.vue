f4437ab06229b600712a1d14499890c2a492fa66
liyl <liyl02@superred.com.cn>
2024-03-27 10:21:19 +0800

    computed get set 调用验证

--- src/components/Person2.vue
+++ src/components/Person2.vue
@@ -21,9 +21,11 @@ let lastName = ref('san')
 // fullName是一个计算属性，可读可写
 let fullName = computed({
   get() {
+    console.log('get()')
     return firstName.value.slice(0, 1).toUpperCase() + firstName.value.slice(1) + '-' + lastName.value
   },
   set(val) {
+    console.log('set()')
     const [str1, str2] = val.split('-')
     firstName.value = str1
     lastName.value = str2
