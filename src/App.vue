a0a1144e2caba71f8651cdb6b1d3850a9ab85875
liyulong <764861214@qq.com>
2024-03-27 16:27:20 +0800

    情况一：监视【ref】定义的【基本类型】数据

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,21 @@
 <template>
     <!-- <Person/> -->
-    <Person2/>
+    <!-- <Person2/> -->
+    <!-- <Person3/> -->
+    <Person4/>
 </template>
 
 <script lang="ts">
     // import Person from './components/Person.vue'
-    import Person2 from './components/Person2.vue'
+    // import Person2 from './components/Person2.vue'
+    // import Person3 from './components/Person3.vue'
+    import Person4 from './components/Person4.vue'
     export default {
         name:'App', //组件名
         // components:{Person}
-        components:{Person2}
+        // components:{Person2}
+        // components:{Person3}
+        components:{Person4}
     }
 </script>
     
---
+++ src/components/Person3.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="person3">
+    <h1>情况一：监视【ref】定义的【基本类型】数据</h1>
+    <h2>当前求和为：{{ sum }}</h2>
+    <button @click="changeSum">点我sum+1</button>
+  </div>
+</template>
+
+<script lang="ts" setup name="Person3">
+import { ref, watch } from 'vue';
+let sum = ref(0)
+
+function changeSum() {
+  sum.value += 1
+}
+
+const stopWatch = watch(sum, (newValue, oldValue) => {
+  console.log('sum变化了', newValue, oldValue)
+  if (newValue >= 10) {
+    stopWatch()
+  }
+})
+
+</script>
+
+<style scoped>
+.person3 {
+  background-color: skyblue;
+  box-shadow: 0 0 10px;
+  border-radius: 10px;
+  padding: 20px;
+}
+
+/* 修改按钮边框和颜色 */
+button {
+  border: 1px solid #de0fc3;
+  /* 添加边框 */
+  background-color: #1ec577;
+  /* 修改背景色 */
+  color: #1208a1;
+  /* 修改字体颜色 */
+  padding: 8px 16px;
+  /* 增加内边距 */
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 14px;
+  margin: 4px 2px;
+  transition: all 0.3s;
+  /* 添加过渡效果 */
+  cursor: pointer;
+  /* 修改光标样式 */
+  border-radius: 4px;
+  /* 添加圆角 */
+}
+
+/* 鼠标悬停时改变按钮样式 */
+button:hover {
+  background-color: #c7de16;
+  /* 改变背景色 */
+  color: #e35151;
+  /* 改变字体颜色 */
+  border-color: #af1c1c;
+  /* 改变边框颜色 */
+}
+</style>
