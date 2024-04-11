2f8331adce5d0ce53a179eea206d33a8fa80b058
liyl <liyl02@superred.com.cn>
2024-04-11 10:14:09 +0800

    备份路由参数params

---
+++ backup/路由后/0411路由参数params/router/index.ts
@@ -0,0 +1,36 @@
+import { createRouter, createWebHistory, createWebHashHistory } from 'vue-router'
+
+import Home from '@/pages/Home.vue'
+import News from '@/pages/News.vue'
+import About from '@/pages/About.vue'
+import Detail from '@/pages/Detail.vue'
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {
+            name: 'Home',
+            path: '/home',
+            component: Home
+        },
+        {
+            name: 'News',
+            path: '/news',
+            component: News,
+            children: [
+                {
+                    name: 'NewsDetail',
+                    path: 'detail/:id/:title/:content',
+                    component: Detail
+                }
+            ]
+        },
+        {
+            name: 'About',
+            path: '/about',
+            component: About
+        }
+    ]
+})
+
+export default router
