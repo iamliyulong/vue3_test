173dbe82de0fcfa8442679de9deaf61d070e01e3
liyl <liyl02@superred.com.cn>
2024-04-15 09:46:58 +0800

    搭建pinia环境

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,13 @@
 import { createApp } from 'vue'
 // 引入App根组件
 import App from './App.vue'
-
+// 引入createPinia，用于创建pinia
+import { createPinia } from 'pinia'
+// 创建pinia
+const pinia = createPinia()
 // 创建一个应用
 const app = createApp(App)
+// 使用pinia
+app.use(pinia)
 // 挂载整个应用到app容器中
 app.mount('#app')
